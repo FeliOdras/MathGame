87ec399f060deaff6f661c9ee7de68bc29bc4adb
odras <ariane.feldmann@odras.de>
2018-12-20 17:52:03 +0100

    created output for lesson by conditions

--- assets/js/matheHelfer.js
+++ assets/js/matheHelfer.js
@@ -3,7 +3,6 @@ let a;
 let b;
 let summe;
 
-function defineVariables() {
 
     // Create random integer for Variables
     a = Math.floor(Math.random() * 21);
@@ -15,16 +14,21 @@ function defineVariables() {
     console.log(a);
     console.log(b);
     console.log(summe);
-}
 
-function createLesson() {
+    if (summe < 21) {
+        window.document.write(a + ' + ' + b + ' = <input type="number">');
+       }else{
+           window.document.write("Das Ergebnis ist groesser als 20.")
+       }
+
+
+function lessonOutput() {
     
 }
 
-defineVariables();
 
-if (summe < 20) {
-    createLesson();
-} else {
-    console.log("Die Summe ist groesser als 20")
-}
+// if (summe < 20) {
+//     lessonOutput();
+// } else {
+//     console.log("Die Summe ist groesser als 20")
+// }
