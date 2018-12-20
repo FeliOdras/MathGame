8ac609a6bbfec97f046a60519e8fde537437f526
odras <ariane.feldmann@odras.de>
2018-12-20 18:08:53 +0100

    Changed Project Name

--- assets/js/mathGame.js
+++ assets/js/mathGame.js
@@ -3,7 +3,7 @@ let a;
 let b;
 let summe;
 
-
+function createAddtionLesson() {
     // Create random integer for Variables
     a = Math.floor(Math.random() * 21);
     b = Math.floor(Math.random() * 21);
@@ -17,10 +17,11 @@ let summe;
 
     if (summe < 21) {
         window.document.write(a + ' + ' + b + ' = <input type="number">');
-       }else{
-           window.document.write("Das Ergebnis ist groesser als 20.")
-       }
+    } else {
+        window.document.write("Das Ergebnis ist groesser als 20.")
+    }
 
+}
 
 // if (summe < 20) {
 //     lessonOutput();
