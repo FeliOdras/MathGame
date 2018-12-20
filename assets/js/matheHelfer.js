69922d212f374af22b70ac5d837d76cc1f93d300
odras <ariane.feldmann@odras.de>
2018-12-20 17:14:46 +0100

    defined conditions (result not greater than 20)

--- assets/js/matheHelfer.js
+++ assets/js/matheHelfer.js
@@ -1,22 +1,30 @@
+// Declare variables
 let a;
 let b;
+let summe;
 
 function defineVariables() {
+
+    // Create random integer for Variables
     a = Math.floor(Math.random() * 21);
     b = Math.floor(Math.random() * 21);
 
-    // console.log(a);
-    // console.log(b);
+    // Define 'summe'
+    summe = a + b;
+
+    console.log(a);
+    console.log(b);
+    console.log(summe);
 }
 
 function createLesson() {
-
+    
 }
 
 defineVariables();
 
-if (a + b < 20) {
+if (summe < 20) {
     createLesson();
 } else {
-    defineVariables();
+    console.log("Die Summe ist groesser als 20")
 }
