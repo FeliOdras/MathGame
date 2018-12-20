8362f387ac95daf80963579a3d4d0ea0957439cc
odras <ariane.feldmann@odras.de>
2018-12-20 17:00:49 +0100

    created function to define variables

--- assets/js/matheHelfer.js
+++ assets/js/matheHelfer.js
@@ -1,2 +1,22 @@
 let a;
 let b;
+
+function defineVariables() {
+    a = Math.floor(Math.random() * 21);
+    b = Math.floor(Math.random() * 21);
+
+    // console.log(a);
+    // console.log(b);
+}
+
+function createLesson() {
+
+}
+
+defineVariables();
+
+if (a + b < 20) {
+    createLesson();
+} else {
+    defineVariables();
+}
