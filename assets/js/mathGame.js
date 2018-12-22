d583ef7ef5201b588401b3614104ba751bfbe2ae
odras <ariane.feldmann@odras.de>
2018-12-22 15:24:49 +0100

    Updated comments

--- assets/js/mathGame.js
+++ assets/js/mathGame.js
@@ -1,43 +1,44 @@
 // Task: Create a math game for Addition with the following requirements:
+// - randomly created addition and substraction exercises
 // - only numbers from 1 - 20 are calculated
-// - also the result must not be greater than 20
+// - also the result must not be greater than 20 and not less 0
 // - create feedback wether the input result is correct or not
 
 function createExercise() {
     // Declare variables
-    let a;
-    let b;
-    let exerciseOutput;
-    let exerciseInput;
-    let exerciseOperator;
-    let exerciseResult;
+    let a; // First number of exercise
+    let b; // Second number of exercise
+    let exerciseOperator; // Mathematical operator + or -
+    let exerciseOutput; // Exercise to solve
+    let exerciseInput; // Solition. Inputr by user
+    let exerciseResult; // Result of exercise
 
-    // Select operator (+ or -)
-    // debugger
     do {
+    // Select random values for exercise variables a and b
     a = Math.floor(Math.random() * 21);
     b = Math.floor(Math.random() * 21);
+    // Select operator randomly  
     exerciseOperator = Math.floor(Math.random() * 2);
 
+    // Define mathematical operators for exercise
     if (exerciseOperator == 0) {
         exerciseOperator = '-';
     } else if (exerciseOperator == 1) {
         exerciseOperator = '+';
     }
 
+    // Calculate Result of exercise
     if (exerciseOperator == '-') {
         exerciseResult = a - b;
     } else if (exerciseOperator == '+') {
         exerciseResult = a + b;
     }
 
-    exerciseOutput = a + exerciseOperator + b;
-    console.log(a + exerciseOperator + b)
-    console.log(exerciseResult)
-    console.log(exerciseOutput)
-
+    exerciseOutput = a + ' ' + exerciseOperator + ' ' + b;
+    // console.log(exerciseOutput)
+    // console.log(exerciseResult)
     }
-    // Repeat until sum of variables is not greater than 20
+    // Repeat until sum or difference of variables is not greater than 20 and not less than 0
     while (exerciseResult > 20 || exerciseResult < 0);
 
     // Build lesson to solve
